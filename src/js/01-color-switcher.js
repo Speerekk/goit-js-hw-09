17f0a47b964fa3a49c11cd166ed839cb8c5d2ea7
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-05-18 16:25:45 +0200

    hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBodyColor, 1000);
+}
+
+function onStopButtonClick() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
